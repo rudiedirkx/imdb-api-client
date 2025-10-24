0a6f44772fd38f89d8073e26d3d88f7f42cbd92c
Rudie Dirkx <github@hotblocks.nl>
2025-10-24 20:25:37 +0200

    fetch in en-US lang

--- src/Client.php
+++ src/Client.php
@@ -43,6 +43,7 @@ class Client {
 			'cookies' => $auth->cookies(),
 			'headers' => [
 				'User-agent' => 'imdb/1.1',
+				'Accept-language' => 'en-US',
 				// 'User-agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
 			],
 			'allow_redirects' => [
